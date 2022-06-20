31af609bbe915130ba9938e97933a4d701254707
ritwik-deshpande <43472373+ritwik-deshpande@users.noreply.github.com>
2022-06-20 21:16:23 +0530

    Time: 1393 ms (27.69%), Space: 26.8 MB (33.84%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -14,7 +14,9 @@ class Solution:
                 r -= 1
             else:
                 ans += pow(2, r-l, mod)
-                print(l, r, ans)
+                # print(l, r, ans)
+                
+        # Basically we choose [3,4,5,6] we fix 3 so n-1C0, n-1C1 ... i.e 2**n -1
                 l += 1
         return ans % mod
                 
