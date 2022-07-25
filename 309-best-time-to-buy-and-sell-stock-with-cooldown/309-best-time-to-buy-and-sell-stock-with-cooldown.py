a3dfb2f69c74d71f88b77793e04fe340220eed34
ritwik-deshpande <43472373+ritwik-deshpande@users.noreply.github.com>
2022-07-26 01:26:29 +0530

    Time: 124 ms (9.45%), Space: 17.4 MB (38.42%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,28 @@
+class Solution:
+    def f(self, i, op):
+        
+        if (i, op) in self.dp:
+            return self.dp[(i, op)]
+        
+        if i >= self.n:
+            return 0
+        
+        if op == 'buy':
+            
+            ret = max(self.f(i + 1, 'sell')  - self.prices[i],  self.f(i + 1, 'buy'))
+        
+        else:
+            ret = max(self.f(i + 2, 'buy')  + self.prices[i],  self.f(i + 1, 'sell'))
+            
+        self.dp[(i, op)] = ret
+        return ret
+    
+    
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        self.prices = prices
+        self.n = len(prices)
+        self.dp = {}
+        
+        return self.f(0, 'buy')
+        
