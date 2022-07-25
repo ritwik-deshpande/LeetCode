684ec589fdb4dee1c3997f7522199b78edb18a68
ritwik-deshpande <43472373+ritwik-deshpande@users.noreply.github.com>
2022-07-26 01:30:20 +0530

    Time: 349 ms (19.05%), Space: 51.7 MB (18.17%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,32 @@
+class Solution:
+    
+    
+    def f(self, i, op, x):
+        
+        if (i, op, x) in self.dp:
+            return self.dp[(i, op, x)]
+        
+        if x > self.k:
+            return 0
+        
+        if i >= self.n:
+            return 0
+        
+        if op == 'buy':
+            
+            ret = max(self.f(i + 1, 'sell', x + 1)  - self.prices[i],  self.f(i + 1, 'buy', x))
+        
+        else:
+            ret = max(self.f(i + 1, 'buy', x)  + self.prices[i],  self.f(i + 1, 'sell', x))
+            
+        self.dp[(i, op, x)] = ret
+        return ret
+    
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        self.prices = prices
+        self.dp = {}
+        self.n = len(prices)
+        self.k = k
+        
+        return self.f(0, 'buy', 0)
+        
