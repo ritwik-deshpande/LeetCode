071a8781275c17c2283dffdf5474583ef682227e
ritwik-deshpande <43472373+ritwik-deshpande@users.noreply.github.com>
2022-08-02 13:19:42 +0530

    Time: 592 ms (5.16%), Space: 17.3 MB (14.35%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def f(self, ptr, target, path):
+        
+        if (ptr, target, tuple(path)) in self.dp:
+            return
+        if target == 0:
+            if sorted(path) not in self.ans:
+                self.ans.append(sorted(path))
+            
+        if ptr == self.N or target < 0:
+            return
+    
+        for i in range(ptr, self.N):
+            
+            self.f(i + 1, target - self.nums[i], path + [self.nums[i]])
+        
+            self.dp[(ptr, target, tuple(path))] = True
+        
+    
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        self.nums = candidates
+        self.N = len(self.nums)
+        self.ans = []
+        self.dp = {}
+        
+        self.f(0, target, [])
+
+        return self.ans
+        
